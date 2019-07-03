08e5aa0f5588b7becae652bfbc77faa656ac7475
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-07-03 18:23:55 -0400

    Updated dictionary to accept random ordering of letters in pattern. Now passes 52/53 tests instead of 7/53.

--- code_wars_practice.py
+++ code_wars_practice.py
@@ -2,20 +2,19 @@ def word_pattern(pattern, string):
 
     listA = string.split(' ')
     new_dict = {}
-    letter = 'a'
     
-    for item in listA:
-        new_dict[item] = new_dict.get(item, letter)
-        if new_dict[item] == letter:
-            letter = chr(ord(letter) + 1)
+    if len(pattern) != len(listA):
+        return False
     
-    for index, item in enumerate(listA):
-        if new_dict[item] != pattern[index]:
+    for index, letter in enumerate(pattern):
+        new_dict[letter] = new_dict.get(letter, listA[index])
+    print(new_dict)
+    print(pattern)
+    print(string)
+
+    for index, item in enumerate(pattern):
+        print('dictionary item: {0} and current pattern: {1}'.format(new_dict[item],pattern[index]))
+        if new_dict[item] != listA[index]:
             return False
             
     return True
-    
-    print(new_dict)
-
-        
-    
