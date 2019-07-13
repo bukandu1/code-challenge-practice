0606ad69963598239aae6034d22405213420d65d
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-07-13 08:39:43 -0400

    Update solution to correct arguments for append.

--- leet_code_practice.py
+++ leet_code_practice.py
@@ -14,24 +14,42 @@
 
 
 class Solution:
+    # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
     def maxDepth(self, root: TreeNode) -> int:
         to_visit_list = []
         depth_set = set()
-        depth = 0
+        
+        if not root:
+            return 0
+            
+        depth = 1
+        to_visit_list.append((root, depth))
+        #print(root.val, depth)
 
         #while items in to_visit list
         while to_visit_list:
             #remove last item in list to review
-            current = to_visit_list.pop()
+            current, depth = to_visit_list.pop()
 
-                #is a leaf?
-                if not current.right and not current.left: 
-                    #store into depth set
+            #is a leaf?
+            if not current.right and not current.left: 
+                #store into depth set
+                depth_set.add(depth)
+                #print("Set contains", depth_set)
+            else:
                 if current.right: 
-                    to_visit_list.append(current.right, depth + 1)
+                    to_visit_list.append((current.right, depth + 1))
                 if current.left: 
-                    to_visit_list.append(current.left, depth + 1)
+                    to_visit_list.append((current.left, depth + 1))
         
+        return max(depth_set)
         return max(depth_set)
                 
         
