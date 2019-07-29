2bbff8b5f05cbdd156eff3ab716fc6c408abc1ff
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-07-29 15:52:48 -0400

    Pseudo code for container with the most water.

---
+++ leet_code/container_with_most_water.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        #set left = 0
+        #set right = last index
+        #max_area = 0
+        
+        #while left and right do not cross
+            #calculate area
+            #current_area = 
+            #max_area = max(current_area, max_area)
+            
+            #decide which pointer to move
+            #minimum item limits the area so increment the min
+        
