59f151a6c9247c280ce384a9d183433deeef5fb3
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-07-30 10:14:19 -0400

    Updated code. Works for [10, 3] and [5, 2].

--- leet_code/divide_two_integers.py
+++ leet_code/divide_two_integers.py
@@ -3,23 +3,33 @@ class Solution:
         dividend_list = list(str(dividend))
         print(dividend_list)
         
-        answer = "0"
+        answer = ""
         
         current_index = 0
-        while current_index < len(dividend_list) - 1:
+        current_dividend_string = dividend_list[current_index]
+        
+        while current_index < len(dividend_list):
             #review first digit in divident list
-            current_dividend_string = dividend_list[current_index]
             current_dividend_int =  int(current_dividend_string)
             
             if current_dividend_int < divisor: 
+                answer += "0"
                 current_dividend_string += dividend_list[current_index + 1]
+    
                 print("string", current_dividend_string)
                 
             else:
                 print("subtract here")
-            
+                counter = 0
+                while current_dividend_int > divisor:
+                    current_dividend_int -= divisor
+                    counter +=1
+                      
+                answer = str(counter)
+                current_dividend_string = str(current_dividend_int)
             
             current_index += 1
     
+
         return int(answer)
         
