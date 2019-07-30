3a50e7aa80b228406bad6d44b0abb84adf7f7962
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-07-30 08:00:53 -0400

    Updating with initial variables and code for area.

--- leet_code/container_with_most_water.py
+++ leet_code/container_with_most_water.py
@@ -4,10 +4,17 @@ class Solution:
         #set right = last index
         #max_area = 0
         
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+        
         #while left and right do not cross
+        while left < right:
             #calculate area
-            #current_area = 
-            #max_area = max(current_area, max_area)
+            height = min(left, right)
+            length = right - left
+            current_area = height * length
+            max_area = max(current_area, max_area)
             
             #decide which pointer to move
             #minimum item limits the area so increment the min
