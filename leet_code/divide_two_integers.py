9abe920b53da8d45ed73a33ae49b6538cd3c2ee8
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-07-30 11:10:33 -0400

    This solution works for [2200, 3] but not for [7, 3].

--- leet_code/divide_two_integers.py
+++ leet_code/divide_two_integers.py
@@ -6,14 +6,18 @@ class Solution:
         answer = ""
         
         current_index = 0
+        counter = 0
         current_dividend_string = dividend_list[current_index]
         
-        while current_index < len(dividend_list):
+        while current_index < len(dividend_list) - 1:
             #review first digit in divident list
             current_dividend_int =  int(current_dividend_string)
             
             if current_dividend_int < divisor: 
-                answer += "0"
+                if counter > 1: 
+                    answer += "0"
+                    counter += 1
+                    current_index += 1
                 current_dividend_string += dividend_list[current_index + 1]
     
                 print("string", current_dividend_string)
@@ -21,14 +25,19 @@ class Solution:
             else:
                 print("subtract here")
                 counter = 0
-                while current_dividend_int > divisor:
+                while current_dividend_int >= divisor:
                     current_dividend_int -= divisor
                     counter +=1
+                    print("counter equals", counter)
                       
-                answer = str(counter)
+                answer += str(counter)
+                print("subtracted answer so far", answer, "current_div", current_dividend_int)
                 current_dividend_string = str(current_dividend_int)
+                current_index += 1
             
-            current_index += 1
+            
+            counter = 0
+            print("index now:",current_index, "answer", answer,"\n")
     
 
         return int(answer)
