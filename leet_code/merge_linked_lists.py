60eb2a2b996e117b987bc6d65110d8d9313fde49
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-08-07 06:17:16 -0700

    Inital commit with empty classes and base case handling empty LL.

---
+++ leet_code/merge_linked_lists.py
@@ -0,0 +1,15 @@
+
+class LinkedList():
+    pass
+
+class Node():
+    pass
+
+
+def merge_two_linked_lists(LL1, LL2):
+    if not LL1 or not LL2:
+        if not LL1: 
+            return LL2
+        else:
+            return LL1
+
