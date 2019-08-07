4e2eaaec38513eb18c54e8fc0ce71a7e69b27138
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-08-07 06:28:40 -0700

    Update file with test case LLs.

--- leet_code/merge_linked_lists.py
+++ leet_code/merge_linked_lists.py
@@ -1,9 +1,13 @@
 
 class LinkedList():
-    pass
+    def __init__(self, head_node=None):
+        self.head = head_node
+        self.tail = head_node
 
 class Node():
-    pass
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
 def merge_two_linked_lists(LL1, LL2):
@@ -12,4 +16,24 @@ def merge_two_linked_lists(LL1, LL2):
             return LL2
         else:
             return LL1
+
+a = Node(1)
+b = Node(4)
+c = Node(8)
+
+a.next = b
+b.next = c
+
+d = Node(1)
+e = Node(2)
+f = Node(7)
+
+d.next = e
+e.next = f
+
+g = Node(8)
+
+LL1 = LinkedList(a)
+LL2 = LinkedList(d)
+
 
