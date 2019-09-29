e489797aa4713b5864771b13c693b0b0280d65d9
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-09-29 07:30:56 -0700

    Naive solution. Working on more optimal solution.

---
+++ leet_code/palindrome_longest_substring.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    # naive solution
+    def longestPalindrome(self, s: str) -> str:
+        #start with first two letters
+        if not s:
+            return ""
+
+        if len(s) < 1:
+            return s
+
+        current_max = s[0]
+
+        for i in range(len(s)):
+            for j in range(i + 1, len(s) + 1):
+                #If a palindome found, compare to current max
+                substring = s[i:j+1]
+                #print(f'substring: {substring} and backward: {substring[::-1]}')
+                if substring == substring[::-1]:
+                    if len(substring) > len(current_max):
+                        current_max = s[i:j+1]
+
+        return current_max
+
+    # more optimal solution
+    def longestPalindromeopt(self, s: str) -> str:
+        return
