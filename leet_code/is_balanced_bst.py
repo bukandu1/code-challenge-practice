934c09f77c17df8ef599cc75e295135b2290d9d5
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-12-02 02:15:30 -0500

    Complete. Needs tests.

--- leet_code/is_balanced_bst.py
+++ leet_code/is_balanced_bst.py
@@ -9,11 +9,47 @@ class Node():
 class Solution:
     def isBalanced(self, node):
 
+
         def helperBalancedHeight(node):
             if not node:
                 return 0
-            h1 = helperBalancedHeight(n.left)
-            h2 = helperBalancedHeight(n.right)
+            h1 = helperBalancedHeight(node.left)
+            h2 = helperBalancedHeight(node.right)
 
             if h1 == -1 or h2 == -1:
                 return -1
+
+            if (abs(h1 - h2) > 1):
+                return -1
+
+            if h1 > h2:
+                return h1 + 1
+
+            return h2 + 1
+
+        if helperBalancedHeight(node) > -1:
+            return True
+        else:
+            return False
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
+n9 = Node(9)
+
+n1.left = n2
+n1.right = n3
+
+n2.left = n4
+n3.left = n6
+n3.right = n7
+n7.left = n8
+n8.left = n9
+
+sol = Solution()
+print(sol.isBalanced(n1))
