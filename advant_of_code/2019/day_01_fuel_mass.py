37212f15546bb572b04d147c0173b52d52a98afd
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-12-12 02:05:58 -0800

    "Will finish in the AM."

--- advant_of_code/2019/day_01_fuel_mass.py
+++ advant_of_code/2019/day_01_fuel_mass.py
@@ -10,18 +10,40 @@ class Solution:
             sum += int(line) // 3 - 2
 
         print(sum)
+        file.close()
 
         return sum
 
+    def deplete_fuel(self, textfile):
+        file = open(textfile, "r")
+        sum = 0 
+        mass = int(file.readline())
+        fuel = mass // 3 - 2
+        while mass >= fuel:
+            
+            sum += fuel
+            if mass >= fuel:
+                mass -= fuel
+            else: 
+                break
+    
+            mass = int(file.readline())
+            fuel = mass // 3 - 2
+
+        file.close()
+        return sum
 
 class TestStringMethods(unittest.TestCase):
+    def setUp(self):
+        self.file = "day_01_fuel_mass.txt"
+
     def test_mass(self):
-        self.assertNotEqual(Solution().fuel_mass("day_01_fuel_mass.txt"), 9827258)
+        self.assertNotEqual(Solution().fuel_mass(self.file), 9827258)
         self.assertEqual(Solution().fuel_mass(
             "day_01_fuel_mass.txt"), 3275518)
 
-    def test_file(self):
-        self.assertTrue('FOO'.isupper())
+    def test_deplete_fuel(self):
+        self.assertEqual(Solution().deplete_fuel(self.file), 79)
         self.assertFalse('Foo'.isupper())
 
     def test_l(self):
