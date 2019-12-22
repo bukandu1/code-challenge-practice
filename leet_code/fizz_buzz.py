f913a077976c4755a9f87386d2324a2e12b46fce
Beverly N Ukandu <beverlyukandu@gmail.com>
2019-12-22 21:34:25 +0100

    Setting up template for problem.

---
+++ leet_code/fizz_buzz.py
@@ -0,0 +1,13 @@
+class Solution:
+    def fizz_buzz(self, num):
+        pass
+
+class TestFizzBuzz(unittest.TestCase):
+    def testFizzBuzzInput(self):
+        # TODO: test length of list for each input
+        # TODO: test result of list for each input
+
+    unittest.main(exit=False)
+
+
+    
