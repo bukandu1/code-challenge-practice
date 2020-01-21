6a412cfce95263c07aa5e8eeec0d6e9b30b4df4a
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-01-21 02:29:37 -0500

    Easy level problem.

---
+++ leet_code/reverse_integer.py
@@ -0,0 +1,31 @@
+class Solution:
+    """
+    Runtime: 24 ms, faster than 93.56% of Python3 online submissions for Reverse Integer.
+    Memory Usage: 12.7 MB, less than 100.00% of Python3 online submissions for Reverse Integer.
+    Next challenges:
+    """
+    def reverse(self, x: int) -> int:
+        #if x <= -2 ** 31 or x >= 2 ** 31 - 1:
+        #    return 0
+        
+        negative = False
+        # determine if x is neg
+        if x < 0:
+            negative = True
+        
+        sum = 0
+        multiplier = 1
+        for digit in str(abs(x)):
+            sum += int(digit) * multiplier
+            multiplier *= 10
+
+        if sum <= -2 ** 31 or sum >= 2 ** 31:
+            return 0
+        
+        return sum if x >= 0 else sum * -1
+
+    
+            
+        
+        
+        
