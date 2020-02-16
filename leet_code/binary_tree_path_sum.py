36027c63ed18c6c8492f28ab1ae27fbdcde1693c
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-02-16 14:29:02 -0500

    Works for all test cases. Needs to make faster and remove global variable use (as discouraged to use per python docs.

--- leet_code/binary_tree_path_sum.py
+++ leet_code/binary_tree_path_sum.py
@@ -11,24 +11,31 @@ class Solution:
         """
         top-down dfs (node passed to children)
         base case: if leaf, calculate its sum
+        
+        
+        Runtime: 48 ms, faster than 22.70% of Python3 online            submissions for Path Sum.
+        Memory Usage: 14.7 MB, less than 100.00% of Python3 online submissions for Path Sum.
         """
         sumFound = [False]
+
+        if not root:
+            return False
 
         def helper(root, runningSum, sum):
             if not root.left and not root.right:
                 if runningSum == sum:
-                    sumFound = [True]
+                    sumFound[0] = [True]
                     print(sumFound, 'runningSum:', runningSum,
                           'current value', root.val, sum)
-                    return sumFound
+                    return
 
             if root.left:
                 helper(root.left, root.left.val + runningSum, sum)
                 #print(runningSum, root.val)
             if root.right:
                 helper(root.right, root.right.val + runningSum, sum)
-                #print(runningSum, root.val)
+                print(runningSum, root.val)
 
-        helper(root, 0, sum)
-        print(sumFound)
-        return sumFound
+        helper(root, root.val, sum)
+        print(sumFound[0], 1)
+        return sumFound[0]
