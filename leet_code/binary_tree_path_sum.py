38090b98620992dedcfe4bff950740425aa3eb13
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-02-16 13:51:45 -0500

    Got global variable to work and true correct result.

--- leet_code/binary_tree_path_sum.py
+++ leet_code/binary_tree_path_sum.py
@@ -5,28 +5,30 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
-    def hasPathSum(self, root: TreeNode, target: int) -> bool:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         """
         top-down dfs (node passed to children)
         base case: if leaf, calculate its sum
         """
-        sumFound = False 
-        
-        def helper(root, runningSum, target):
+        sumFound = [False]
+
+        def helper(root, runningSum, sum):
             if not root.left and not root.right:
-                if runningSum + root.val == target:
-                    sumFound = True
-                    print(sumFound, runningSum, root.val)
+                if runningSum == sum:
+                    sumFound = [True]
+                    print(sumFound, 'runningSum:', runningSum,
+                          'current value', root.val, sum)
                     return sumFound
-                else:
-                    return
+
             if root.left:
-                helper(root.left, root.left.val + runningSum, target)
+                helper(root.left, root.left.val + runningSum, sum)
+                #print(runningSum, root.val)
             if root.right:
-                helper(root.right, root.right.val + runningSum, target)
-                
-            
-        sumFound = helper(root, 0, target)
+                helper(root.right, root.right.val + runningSum, sum)
+                #print(runningSum, root.val)
+
+        helper(root, 0, sum)
+        print(sumFound)
         return sumFound
-        
