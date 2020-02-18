3ecc51df289cd773379aa66a470cd1d2c0f0f6e6
Beverly N Ukandu <beverlyukandu@gmail.com>
2020-02-18 12:15:29 -0500

    Prelim code. Need to test with all edge cases.

--- leet_code/group_anagrams.py
+++ leet_code/group_anagrams.py
@@ -20,3 +20,21 @@ class Solution:
             # else: create new key and add word to new list
 
         # add results to result list by looping through keys
+
+        word_dict = {}
+        for word in words_list:
+            sorted_word = ''.join(sorted(word))
+            print(sorted_word)
+            if sorted_word not in word_dict:
+                word_dict[sorted_word] = [word]
+            else:
+                word_dict[sorted_word].append(word)
+        # print(word_dict)
+
+        return [word_dict[key] for key in word_dict]
+
+s = Solution()
+
+# TEST
+s.group_anagrams(["eat", "tea", "tan", "ate", "nat", "bat"])
+s.group_anagrams([]))
